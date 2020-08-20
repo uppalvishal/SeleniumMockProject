ec3c3f8848f9afd5d5a56f2425b0bb875bab5734
vuppal <vuppal@NSWSYDNLP192.planit.local>
2020-08-20 16:52:47 +1000

    test

--- testngBatchFile.bat
+++ testngBatchFile.bat
@@ -1,2 +1,2 @@
-java -cp https://github.com/uppalvishal/SeleniumMockProject/tree/master/lib/* org.testng.TestNG testngscripts.xml
+java -cp https://github.com/uppalvishal/SeleniumMockProject/tree/master/lib/ org.testng.TestNG testngscripts.xml
 pause
