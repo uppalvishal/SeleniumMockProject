47e06ef94ae28e252d199793920a3c3ed38c2995
vuppal <vuppal@NSWSYDNLP192.planit.local>
2020-08-31 11:41:15 +1000

    test

--- src/test/java/com/projectname/tests/Test_001.java
+++ src/test/java/com/projectname/tests/Test_001.java
@@ -53,6 +53,7 @@ public class Test_001 extends BaseTestSuite {
 		System.out.println("testing feature branch2");
 		System.out.println("testing feature branch3003");
 		System.out.println("testing feature branch3005 vishal");
+		System.out.println("testing feature branch3006 vishal");
 		
 
 		//Fin
